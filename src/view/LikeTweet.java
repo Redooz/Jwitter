a51210b83054e62efc68a35ba5741e4f3563b7cb
Nicolás Olmos <yohannicolas2003@gmail.com>
2022-07-20 19:19:37 -0500

    Change Like Tweet gui style

--- src/view/LikeTweet.java
+++ src/view/LikeTweet.java
@@ -8,6 +8,7 @@ import javax.swing.*;
  */
 public class LikeTweet extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel font;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton likeBtn;
     private javax.swing.JList<String> tweetsJlist;
@@ -21,39 +22,63 @@ public class LikeTweet extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        font = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tweetsJlist = new javax.swing.JList<>();
         likeBtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
+        font.setBackground(new java.awt.Color(21, 32, 43));
+
+        tweetsJlist.setBackground(new java.awt.Color(21, 32, 43));
+        tweetsJlist.setForeground(new java.awt.Color(255, 255, 255));
         tweetsJlist.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         tweetsJlist.setLayoutOrientation(javax.swing.JList.VERTICAL_WRAP);
         jScrollPane1.setViewportView(tweetsJlist);
 
+        likeBtn.setBackground(new java.awt.Color(43, 141, 215));
+        likeBtn.setForeground(new java.awt.Color(255, 255, 255));
+        likeBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/like16.png"))); // NOI18N
         likeBtn.setText("Like");
+        likeBtn.setBorderPainted(false);
+
+        javax.swing.GroupLayout fontLayout = new javax.swing.GroupLayout(font);
+        font.setLayout(fontLayout);
+        fontLayout.setHorizontalGroup(
+            fontLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(fontLayout.createSequentialGroup()
+                .addGap(165, 165, 165)
+                .addComponent(likeBtn)
+                .addContainerGap(164, Short.MAX_VALUE))
+            .addGroup(fontLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(fontLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
+                    .addContainerGap()))
+        );
+        fontLayout.setVerticalGroup(
+            fontLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, fontLayout.createSequentialGroup()
+                .addContainerGap(279, Short.MAX_VALUE)
+                .addComponent(likeBtn)
+                .addContainerGap())
+            .addGroup(fontLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(fontLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(46, Short.MAX_VALUE)))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(likeBtn)
-                .addGap(173, 173, 173))
+            .addComponent(font, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(19, 19, 19)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 255, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(likeBtn)
-                .addContainerGap(12, Short.MAX_VALUE))
+            .addComponent(font, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
